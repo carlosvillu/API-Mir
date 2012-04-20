b1e052cbdbf4f57fd6c10d265bcf486691f74589
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2012-04-20 18:08:51 +0200

    #1 Create API site

--- server.js
+++ server.js
@@ -1,30 +1,7 @@
+require('coffee-script');
+http = require('http');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http');
-
-var app = express();
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
+var app = require('./app');
 
 http.createServer(app).listen(3000);
 
